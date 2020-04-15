87f7872e9505014bfeb8f3eed8d7cec61160aa41
Diego Leon Sanchez Rios <Diego@MacBook-Air-de-Diego-2.local>
2020-04-15 15:41:18 -0500

    git branch littlefeature

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const obj = {
     name0: 'Sally',
     name1: 'Bob',
     name2: 'Diego',
+    name3: 'git branch',
+    name4: 'git branch littlefeature',
+    name5: 'git checkout littlefeature',
 }
 
 Object.values(obj).forEach(element => {
